d511c55b09a4bfd4a7d6ad169b2ead05755a6620
StopThatTalace <0x.88ta.ko@gmail.com>
2024-12-04 15:24:29 +0100

    blud is discovering part2 of every chall 😹

--- Day 1 - Historian Hysteria/main.py
+++ Day 1 - Historian Hysteria/main.py
@@ -21,11 +21,22 @@ def get_total_distance(distance_1: list[int], distance_2: list[int]) -> int:
 
     return total_distance
 
+def get_similarity(distance_1: list[int], distance_2: list[int]):
+    total_similarity = 0
+
+    for number in distance_1:
+        total_similarity += number * distance_2.count(number)
+
+    return total_similarity
+
 
 def main():
     distance_1, distance_2 = get_lists_from_file("input.txt")
-    total_distance = get_total_distance(distance_1, distance_2)
-    print(total_distance)
+    print(get_total_distance(distance_1, distance_2))
+    print(get_similarity(distance_1, distance_2))
+
+
+
 
 if __name__ == "__main__":
     main()
